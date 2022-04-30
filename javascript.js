899dea2610140fb553bd108d20b3af72630662c8
Grant Deedler <grantdeedler@gmail.com>
2022-04-29 22:13:56 -0400

    Add logic for rock paper scissors round

--- javascript.js
+++ javascript.js
@@ -7,6 +7,28 @@ function computerPlay(){
     }
 }
 
-for(let i = 0; i < 100; i++){
-    console.log(computerPlay());
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection === computerSelection){
+        return `It's a tie! You both picked ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}!`
+    }
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'paper') return 'You lose! Paper beats Rock';
+        else if(computerSelection === 'scissors') return 'You win! Rock beats Scissors!';
+    } else if(playerSelection === 'paper'){
+        if(computerSelection === 'rock') return 'You win! Paper beats Rock!';
+        else if(computerSelection === 'scissors') return 'You lose! Scissors beats Paper!';
+    } else if(playerSelection === 'scissors'){
+        if(computerSelection === 'rock') return 'You lose! Rock beats Scissors!';
+        else if(computerSelection === 'paper') return 'You win! Scissors beats Paper!';
+    }
+    return 'Oops, an invalid selection was made.';
+}
+
+for(let i = 0; i < 50; i++){
+    let play1 = computerPlay();
+    let play2 = computerPlay();
+    console.log(play1 +' vs ' + play2);
+    console.log(playRound(play1,play2));
 }
