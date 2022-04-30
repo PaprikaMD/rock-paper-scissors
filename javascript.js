02f82cf6692240c51d1a5648f9040ec43f5046c8
Grant Deedler <grantdeedler@gmail.com>
2022-04-29 22:20:19 -0400

    Refactor game logic to be more useful

--- javascript.js
+++ javascript.js
@@ -11,24 +11,22 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     if(playerSelection === computerSelection){
-        return `It's a tie! You both picked ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}!`
+        return 0;
     }
     if(playerSelection === 'rock'){
-        if(computerSelection === 'paper') return 'You lose! Paper beats Rock';
-        else if(computerSelection === 'scissors') return 'You win! Rock beats Scissors!';
+        if(computerSelection === 'paper') return -1;
+        else if(computerSelection === 'scissors') return 1;
     } else if(playerSelection === 'paper'){
-        if(computerSelection === 'rock') return 'You win! Paper beats Rock!';
-        else if(computerSelection === 'scissors') return 'You lose! Scissors beats Paper!';
+        if(computerSelection === 'rock') return 1;
+        else if(computerSelection === 'scissors') return -1;
     } else if(playerSelection === 'scissors'){
-        if(computerSelection === 'rock') return 'You lose! Rock beats Scissors!';
-        else if(computerSelection === 'paper') return 'You win! Scissors beats Paper!';
+        if(computerSelection === 'rock') return -1;
+        else if(computerSelection === 'paper') return 1;
     }
-    return 'Oops, an invalid selection was made.';
+    console.log('Oops, an invalid selection was made. Try again');
+    return 0;
 }
 
-for(let i = 0; i < 50; i++){
-    let play1 = computerPlay();
-    let play2 = computerPlay();
-    console.log(play1 +' vs ' + play2);
-    console.log(playRound(play1,play2));
+function game() {
+    
 }
