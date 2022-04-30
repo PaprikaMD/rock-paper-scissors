0ff212ed4e18e92c1fb98fe3d5058d1230bf8896
Grant Deedler <grantdeedler@gmail.com>
2022-04-29 22:32:08 -0400

    Add most basic game logic and cleanText function

--- javascript.js
+++ javascript.js
@@ -27,6 +27,26 @@ function playRound(playerSelection, computerSelection){
     return 0;
 }
 
+function cleanText(string){
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
 function game() {
-    
+    let playerWins = 0;
+    let computerWins = 0;
+    console.log("Let's play a game! First to 3 wins");
+    while((playerWins<3) && (computerWins < 3)){
+        let playerSelection = cleanText(prompt('Enter your play: '));
+        computerSelection = computerPlay();
+        let result = playRound(playerSelection, computerSelection);
+        if(result === 1){
+            playerWins++;
+            alert(`You win! ${playerSelection} beats ${computerSelection}!`);
+        } else if (result === -1){
+            computerWins++;
+            alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
+        } else if (result === 0){
+            alert(`It's a tie! You both picked ${playerSelection}`);
+        }
+    }
 }
