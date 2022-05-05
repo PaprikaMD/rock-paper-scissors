5b81419d3f98e3a144762fb5d6527db859d97ace
Grant Deedler <grantdeedler@gmail.com>
2022-05-05 15:40:39 -0400

    Add basic CSS and DOM manipulation to game

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 function computerPlay(){
     let rand = Math.floor(Math.random()*3);
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => button.style.backgroundColor = 'black')
     switch(rand){
-        case 0: return 'Rock';
-        case 1: return 'Paper';
-        case 2: return 'Scissors';
+        case 0: buttons[0].style.backgroundColor = 'rgb(60, 0, 0)';
+        return 'Rock';
+        case 1: buttons[1].style.backgroundColor = 'rgb(60, 0, 0)';
+        return 'Paper';
+        case 2: buttons[2].style.backgroundColor = 'rgb(60, 0, 0)';
+        return 'Scissors';
     }
 }
 
@@ -31,28 +36,42 @@ function cleanText(string){
     return string.length>0 ? string[0].toUpperCase() + string.slice(1).toLowerCase() : 'empty string';
 }
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    console.log("Let's play a game! First to 3 wins");
-    while((playerWins<3) && (computerWins < 3)){
-        let playerSelection = cleanText(prompt('Enter your play: '));
-        computerSelection = computerPlay();
-        console.log(`Computer chooses ${computerSelection}`);
-        let result = playRound(playerSelection, computerSelection);
-        if(result === 1){
-            playerWins++;
-            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-        } else if (result === -1){
-            computerWins++;
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-        } else if (result === 0){
-            console.log(`It's a tie! You both picked ${playerSelection}`);
-        }
-        console.log(`Score is \nPlayer: ${playerWins}\nComputer: ${computerWins}`);
+function updatePage(result){
+    let playerScore = document.querySelector('.player-score');
+    let computerScore = document.querySelector('.computer-score');
+    let message = document.querySelector('.message');
+    if(result === 1){
+        playerScore.textContent++;
+        message.textContent = 'Player wins!';
     }
-    if(playerWins === 3) console.log('Game over! You win!');
-    else console.log('Game over, the computer wins!');
+    if(result === -1){
+        computerScore.textContent++;
+        message.textContent = 'Computer wins!';
+    }
+    if(result === 0){
+        message.textContent = 'It\'s a tie!';
+    }
+    if(playerScore.textContent == 5){
+        document.querySelector('.winner').textContent = 'PLAYER WINS!';
+        return true;
+    }
+    if(computerScore.textContent == 5){
+        document.querySelector('.winner').textContent = 'COMPUTER WINS!';
+        return true;
+    }
+    return false;
+}
+
+function game() {
+    const buttons = document.querySelectorAll('button');
+    let gameOver = false;
+    console.log(buttons);
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            let result = playRound(button.id, computerPlay());
+            gameOver = updatePage(result);
+        })
+    })
 }
 
 game();
